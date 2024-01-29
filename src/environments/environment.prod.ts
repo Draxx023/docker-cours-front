d49f2fa152f9c8aaaa6b343e275e08cd6ef66897
Draxx023 <ayoub2001@hotmail.it>
2024-01-29 14:47:31 +0100

    build push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import {Environment} from "./environment.interface";
 
 export const environment: Environment = {
   production: true,
-  backendUrl: 'http://localhost:8080'
+  backendUrl: '{{ BACKEND_URL }}'
 };
